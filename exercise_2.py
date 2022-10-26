96f070d525ee0ef476680e641d50c06938230d05
Konstantinos Koliopoulos <konst.koliop@gmail.com>
2022-10-26 23:41:57 +0300

    First two exercises

---
+++ exercise_2.py
@@ -0,0 +1,16 @@
+# Τι θα εκτυπώσει το πρόγραμμα αυτό (με τη σειρά);
+
+x = 9
+y = 7
+if x > y:
+    print(x + y)
+else:
+    print(x - y)
+y = x
+x = 0
+if x == y:
+    print(x * y)
+elif x < y:
+    print(y)
+else:
+    print(x)
