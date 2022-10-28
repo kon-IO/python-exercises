0ea2b934586df586c019e1442ed1c380aa1184f7
Konstantinos Koliopoulos <konst.koliop@gmail.com>
2022-10-28 20:01:56 +0200

    Added third exercise

---
+++ exercise_3.py
@@ -0,0 +1,17 @@
+# Τι θα εκτυπώσει το παρακάτω πρόγραμμα (με τη σειρά);
+
+x = 0
+h = 3
+if x > h:
+    print('oops')
+else:
+    if 0 > 9:
+        for i in range(9):
+            print('lol')
+    elif 9 == 9:
+        for i in range(10):
+            print('haha')
+    else:
+        print('else')
+print('vrooom')
+            
